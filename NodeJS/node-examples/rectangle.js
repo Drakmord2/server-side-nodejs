026135ce0ab93d5e4161432a2a9b16b01039bf46
Rubens Carneiro <rubenseuclides@gmail.com>
2018-01-11 02:20:09 -0300

    Code cleaning.

--- NodeJS/node-examples/rectangle.js
+++ NodeJS/node-examples/rectangle.js
@@ -4,15 +4,12 @@ module.exports = rectangle;
 function rectangle (x, y, callback) {
 
     if (x <= 0 || y <= 0) {
-        setTimeout(
-            () => callback(
-                    new Error("Rectangle dimensions should be greater than zero:  l = " + x + ",  and b = " + y),
-                    null
-                ),
-            1000
-        );
 
-    } else {
+        let err = new Error("Rectangle dimensions should be greater than zero:  l = " + x + ",  and b = " + y);
+
+        setTimeout( () => callback(err, null), 1000);
+    }
+    else {
 
         let prop = {
             perimeter:  () => (2*(x+y)),
