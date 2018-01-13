f3d22ff2f2e43cd91c32e8ee76136347e2ae0542
Rubens Carneiro <rubenseuclides@gmail.com>
2018-01-13 01:19:38 -0300

    Express simple REST

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -1,18 +1,61 @@
 
-const express   = require('express');
-const http      = require('http');
-const morgan    = require('morgan');
+const express       = require('express');
+const http          = require('http');
+const morgan        = require('morgan');
+const bodyParser    = require('body-parser');
 
-const hostname = 'node';
-const port = '9090';
-
-const app = express();
+const hostname  = 'node';
+const port      = '9090';
+const app       = express();
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
-app.use(
-    express.static(__dirname+'/public')
-);
+// Express Router
+app.all('/dishes', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end('We will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Put operation not supported on dishes.');
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all the dishes!');
+});
+
+// Routes with parameters
+app.get('/dishes/:dishId', (req, res, next) => {
+    res.end('We will send details of the dish: ' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+    res.statusCode = 403;
+    res.end('Post operation not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('We will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
 
 app.use((req, res, next) => {
     res.statusCode = 200;
