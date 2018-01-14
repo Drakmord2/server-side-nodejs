55a998fdb9688baaab044b9c41cfbcf02a34887a
Rubens Carneiro <rubenseuclides@gmail.com>
2018-01-14 20:56:10 -0300

    Code cleaning

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -1,17 +1,18 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var index         = require('./routes/index');
-var users         = require('./routes/users');
-var dishRouter    = require('./routes/dishRouter');
-var promoRouter   = require('./routes/promoRouter');
-var leaderRouter  = require('./routes/leaderRouter');
+const express         = require('express');
+const path            = require('path');
+const favicon         = require('serve-favicon');
+const logger          = require('morgan');
+const cookieParser    = require('cookie-parser');
+const bodyParser      = require('body-parser');
 
-var app = express();
+const index         = require('./routes/index');
+const users         = require('./routes/users');
+const dishRouter    = require('./routes/dishRouter');
+const promoRouter   = require('./routes/promoRouter');
+const leaderRouter  = require('./routes/leaderRouter');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,15 +26,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use('/',            index);
+app.use('/users',       users);
+app.use('/dishes',      dishRouter);
+app.use('/promotions',  promoRouter);
+app.use('/leaders',     leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
