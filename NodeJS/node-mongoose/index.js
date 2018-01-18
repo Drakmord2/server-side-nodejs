01e52b7fd10e36552d400f73c26b4b1930a7e2e9
Rubens Carneiro <rubenseuclides@gmail.com>
2018-01-18 00:17:16 -0300

    Mongoose Part 2

--- NodeJS/node-mongoose/index.js
+++ NodeJS/node-mongoose/index.js
@@ -12,28 +12,26 @@ connect.then((conn) => {
 
     console.log('\n- Connected correctly to server\n');
 
-    const newDish = Dishes(
+    Dishes.create(
         {
             name: "Pizza",
             description: "Cheese and Pepperoni"
         }
-    );
+    )
+    .then((result) => {
+        console.log('- Inserted dish:\n', result);
 
-    newDish.save()
-        .then((result) => {
-            console.log('- Inserted dish:\n', result);
+        return Dishes.find({}).exec();
+    })
+    .then((dishes) => {
+        console.log('\n- Found dishes:\n', dishes);
 
-            return Dishes.find({}).exec();
-        })
-        .then((dishes) => {
-            console.log('\n- Found dishes:\n', dishes);
-
-            return db.collection('dishes').drop();
-        })
-        .then(() => {
-            console.log('\n- Dropped collection and closed connection\n');
-            return client.close();
-        })
-        .catch(console.log);
+        return db.collection('dishes').drop();
+    })
+    .then(() => {
+        console.log('\n- Dropped collection and closed connection\n');
+        return client.close();
+    })
+    .catch(console.log);
 
 }).catch(console.log);
